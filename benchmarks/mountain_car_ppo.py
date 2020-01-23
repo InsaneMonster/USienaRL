a92f4026373f88ca16c986eec2d75a29dc18258b
InsaneMonster <insanemst@gmail.com>
2020-01-23 12:13:59 +0100

    Updated benchmarks experiments for 0.6.5

--- benchmarks/mountain_car_ppo.py
+++ benchmarks/mountain_car_ppo.py
@@ -113,7 +113,7 @@ def run(workspace: str,
                    testing_episodes, test_cycles,
                    render_training, render_validation, render_test,
                    workspace, __file__,
-                   logger, None, experiment_iterations,
+                   logger, None, None, experiment_iterations,
                    None,
                    plot_sample_density_training_episodes, plot_sample_density_validation_episodes)
     run_experiment(experiment_explore,
@@ -123,7 +123,7 @@ def run(workspace: str,
                    testing_episodes, test_cycles,
                    render_training, render_validation, render_test,
                    workspace, __file__,
-                   logger, None, experiment_iterations,
+                   logger, None, None, experiment_iterations,
                    None,
                    plot_sample_density_training_episodes, plot_sample_density_validation_episodes)
 
